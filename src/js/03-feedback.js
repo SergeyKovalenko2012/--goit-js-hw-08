8690bbb8cd03dd14fe146a7956423a80b5afb9b6
SergeyKovalenko2012 <sergeykovalenko201290@gmail.com>
2022-04-16 15:42:00 +0300

    hw08added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -19,23 +19,28 @@ form.addEventListener('submit', function(evt) {
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 });
+
+email.addEventListener('input', throttle(onTextereaInput, 500));
 message.addEventListener('input', throttle(onTextereaInput, 500));
+
 popularMessage();
 
-function onTextereaInput(evt) {
+function onTextereaInput() {
   const data = {
-    email: email.value,
     message: message.value,
+    email: email.value,
   };
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 function popularMessage() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    console.log(savedMessage);
     const data = JSON.parse(savedMessage);
     email.value = data.email;
     message.value = data.message;
+
+    console.log(savedMessage);
   }
 }
